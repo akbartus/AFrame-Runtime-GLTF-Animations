cb67603c1274a482634698ca67c1a7f50b4c2cfc
Akbar S <54757270+akbartus@users.noreply.github.com>
2022-06-05 18:01:10 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+ // Variables        
+ var objectMesh;
+ var selectedAnimation;
+ var model = document.getElementById("model");
+
+ // Animations Controls
+ function idle() {
+     selectedAnimation = "idle";
+     doAnimation();
+ }
+ function look() {
+     selectedAnimation = "look";
+     doAnimation();
+ }
+ function sad() {
+     selectedAnimation = "sad";
+     doAnimation();
+ }
+ function pushUpAndJump() {
+     selectedAnimation = "pushUpWithJump";
+     doAnimation();
+ }
+ function twist() {
+     selectedAnimation = "twist";
+     doAnimation();
+ }
+ function kick() {
+     selectedAnimation = "kick";
+     doAnimation();
+ }
+
+ // Load 3d model with idle animation once!
+ model.setAttribute("gltf-model", 'url(animations/idle.glb)');
+ model.addEventListener("model-loaded", function (event) {
+     objectMesh = model.getObject3D("mesh");
+ });
+
+
+ // Three.js, load animations from links 
+ var loader = new THREE.GLTFLoader();
+ function doAnimation() {
+     var link = "animations/" + selectedAnimation + ".glb";
+     loader.load(link, function (gltf) {
+         var newModel = gltf.scene;
+         var mixer = new THREE.AnimationMixer(newModel);
+         var currentAnimation = mixer.clipAction(gltf.animations[0]);
+         objectMesh.animations[0] = currentAnimation._clip;
+         model.setAttribute('animation-mixer', "clip:" + selectedAnimation + "; crossFadeDuration:1;");
+     });
+ }
